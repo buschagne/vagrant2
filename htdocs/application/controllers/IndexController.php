9780ad831e2cb77844c93ecec3f3bdfe1423cf86
webmaster <webmaster@L05MAC00.local>
2013-01-24 22:11:02 +0100

    project

---
+++ htdocs/application/controllers/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+       $this->view->naam = "Xavier";
+    }
+
+    public function productenAction()
+    {
+        $this->view->test = "testje";
+    }
+
+    public function sitemapAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
